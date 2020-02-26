81eaa538cab360e95f9d4389bda01963f537ddf3
KundaiMuchuchu <61458165+KundaiMuchuchu@users.noreply.github.com>
2020-02-26 12:46:44 +0200

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,7 @@
+n=float(input("Enter the average height of ten people: "))
+a=[]
+for i in range(0,10):
+    height=float(input("Enter height: "))
+    a.append(height)
+avg=sum(a)/n
+print("Average height 0f 10 people is",round(avg,2))
